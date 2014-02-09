4352bfb262a63409736badafd611fc9cda99177a
otakustay <otakustay@gmail.com>
2014-02-09 22:48:44 +0800

    更新版本为0.8.0-alpha.2

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@
 define(
     function (require) {
         var main = {
-            version: '0.8.0-alpha.1',
+            version: '0.8.0-alpha.2',
 
             enableExtensions: function () {
                 // 加载扩展
