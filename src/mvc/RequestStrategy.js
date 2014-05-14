95dc5fd4eafe0366c44703af447bfee4f1f95586
otakustay <otakustay@gmail.com>
2014-05-14 12:25:34 +0800

    添加RequestStrategy
    Fixed #33

---
+++ src/mvc/RequestStrategy.js
@@ -0,0 +1,65 @@
+/**
+ * UB RIA Base
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ *
+ * @ignore
+ * @file 请求处理策略类
+ * @author otakustay
+ */
+define(
+    function (require) {
+        /**
+         * 请求处理策略类
+         *
+         * 该类用于按一定规则处理请求时的URL、请求名称、请求参数等，通常每个项目会根据自身的前后端接口约定有一个通用的实现
+         *
+         * 默认实现是不对输入进行任何处理，添加`"json"`作为默认响应格式
+         *
+         * @constructor
+         */
+        function RequestStrategy() {
+        }
+
+        /**
+         * 处理请求名称，具体业务可以使用此方法对请求名称进行一些替换操作，
+         * 如可以根据当前对象的`entityName`属性为请求名称加上前缀等
+         *
+         * @param {string} name 当前请求的名称
+         * @return {string}
+         * @protected
+         */
+        RequestStrategy.prototype.formatName = function (name) {
+            return name;
+        };
+
+        /**
+         * 处理请求的URL，具体业务可以使用此方法对请求的URL进行一些替换操作，
+         * 如可以根据当前对象的`entityName`来生成通用的URL等
+         *
+         * @param {string} url 当前请求的URL
+         * @return {string}
+         * @protected
+         */
+        RequestStrategy.prototype.formatURL = function (url) {
+            return url;
+        };
+
+        /**
+         * 处理请求参数
+         *
+         * @param {Object} options 请求的参数
+         * @return {Object}
+         * @protected
+         */
+        RequestStrategy.prototype.formatOptions = function (options) {
+            // 默认使用JSON作为响应格式
+            if (!options.dataType) {
+                options.dataType = 'json';
+            }
+
+            return options;
+        };
+
+        return RequestStrategy;
+    }
+);
