3540c9ef427803f14d29586d85f8385247c0c45d
dengxinxin <dengxinxin@baidu.com>
2014-05-20 13:47:27 +0800

    tpl.js插件支持自定义标签的依赖分析

--- src/tpl.js
+++ src/tpl.js
@@ -77,10 +77,10 @@ define(
             var dependencies = [];
             var defined = {};
 
-            var regex = /data-ui-type="(\w+)"/g;
+            var regex = /<\s*esui-([\w-]+)[^>]*>|data-ui-type="(\w+)"/g;
             var match = regex.exec(text);
             while (match) {
-                var type = match[1];
+                var type = match[1] && util.pascalize(match[1]) || match[2];
                 if (!defined[type]) {
                     defined[type] = true;
 
