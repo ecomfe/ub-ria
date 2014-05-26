f7a622e7a2730aad78d423d150098794dbc70bf4
otakustay <otakustay@gmail.com>
2014-05-26 16:41:44 +0800

    更新版本至1.0.0-alpha.4
    本版本开始，都先更新版本号再开发相应功能，最后发布

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@
 define(
     function (require) {
         var main = {
-            version: '1.0.0-alpha.3',
+            version: '1.0.0-alpha.4',
 
             enableExtensions: function () {
                 // 加载扩展
