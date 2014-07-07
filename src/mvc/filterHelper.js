21f81040ea81777ab2fccac46be06384bda55229
dengxinxin <dengxinxin@baidu.com>
2014-07-07 20:05:07 +0800

    抽象筛选功能

---
+++ src/mvc/filterHelper.js
@@ -0,0 +1,22 @@
+/**
+ * UB RIA Base
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ *
+ * @ignore
+ * @file 筛选控件辅助函数
+ * @author Exodia
+ */
+define(
+    function (require) {
+        var u = require('underscore');
+
+        var helper = {};
+        var select = helper.select = {};
+        select.getText = function (filter) {
+            var item = u.find(filter.datasource, function (item) { return item.value == filter.value; });
+            return item && item.text || '';
+        };
+
+        return helper;
+    }
+);
