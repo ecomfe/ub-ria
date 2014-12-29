22bc4a9eaf67cb0669e95e5de753755d4fa01391
srhb18 <srhb18@163.com>
2014-12-29 14:58:18 +0800

    增加TogglePanel对Overlay展开形式的支持

--- src/ui/TogglePanel.js
+++ src/ui/TogglePanel.js
@@ -1,10 +1,9 @@
 /**
  * ESUI (Enterprise Simple UI)
- * Copyright 2014 Baidu Inc. All rights reserved.
  *
  * @ignore
  * @file 折叠控件
- * @author wangyaqiong
+ * @author wangyaqiong, liyidong(srhb18@gmail.com)
  */
 
 define(
@@ -14,6 +13,7 @@ define(
         var ui = require('esui');
 
         require('esui/Panel');
+        require('esui/Overlay');
 
         /**
          * 折叠控件
@@ -33,7 +33,8 @@ define(
          */
         TogglePanel.prototype.initOptions = function (options) {
             var defaults = {
-                expanded: false
+                expanded: false,
+                position: 'fixed'
             };
 
             var properties = lib.extend(defaults, options);
@@ -52,13 +53,43 @@ define(
             var titleElem = children[0];
             var contentElem = children[1];
 
+            // 初始化Title部分的DOM结构
+            initTitle.call(this, titleElem);
+
+            // 初始化content部分的DOM结构
+            var position = this.position;
+            if (position === 'fixed') {
+                // 占位
+                initContentPanel.call(this, contentElem);
+            }
+            else {
+                // 不占位
+                initContentOverlay.call(this, contentElem);
+            }
+        };
+
+        /**
+         * 初始化Title部分的DOM结构
+         *
+         * @inner
+         * @param {Object} titleElem Title的DOM对象
+         */
+        function initTitle(titleElem) {
             var titlePanel = ui.create('Panel', { main: titleElem });
             this.helper.addPartClasses('title', titlePanel.main);
             this.addChild(titlePanel, 'title');
             titlePanel.render();
             this.set('title', titleElem && titleElem.innerHTML);
             titlePanel.helper.addDOMEvent(titlePanel.main, 'click', lib.bind(onToggle, this));
+        }
 
+        /**
+         * 按Panel模式初始化Content部分的DOM结构
+         *
+         * @inner
+         * @param {Object} contentElem content的DOM对象
+         */
+        function initContentPanel(contentElem) {
             var options = {
                 main: contentElem,
                 childName: 'content',
@@ -70,17 +101,69 @@ define(
             this.helper.addPartClasses('content', contentPanel.main);
             this.addChild(contentPanel, 'content');
             contentPanel.render();
-        };
+        }
 
+        /**
+         * 按Overlay模式初始化Content部分的DOM结构
+         *
+         * @inner
+         * @param {Object} contentElem content的DOM对象
+         */
+        function initContentOverlay(contentElem) {
+            var overlayMain = this.helper.createPart('layer', 'div');
+            lib.addClass(overlayMain, this.helper.getPartClassName('layer'));
+
+            var options = {
+                main: contentElem,
+                childName: 'content',
+                attachedDOM: lib.getChildren(this.main)[0],
+                attachedLayout: 'bottom,left',
+                autoClose: false,
+                viewContext: this.viewContext,
+                renderOptions: this.renderOptions
+            };
+            var contentLayer = ui.create('Overlay', options);
+
+            this.helper.addPartClasses('content', contentLayer.main);
+            this.addChild(contentLayer, 'content');
+            contentLayer.render();
+        }
+
+        /**
+         * 点击Title区域的句柄
+         *
+         * @inner
+         */
         function onToggle() {
             this.toggleContent();
         }
 
         /**
          * 切换展开/收起状态
+         *
+         * @inner
          */
         TogglePanel.prototype.toggleContent = function () {
-            this.toggleState('expanded');
+            var position = this.position;
+
+            if (position === 'fixed') {
+                // 占位模式
+                this.toggleState('expanded');
+            }
+            else {
+                // 浮层模式
+                var contentLayer = this.getChild('content');
+
+                if (this.isExpanded()) {
+                    this.removeState('expanded');
+                    contentLayer.hide();
+                }
+                else {
+                    this.toggleState('expanded');
+                    contentLayer.show();
+                }
+            }
+
             this.fire('change');
         };
 
